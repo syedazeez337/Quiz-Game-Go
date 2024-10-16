45be631372dbe2f8749e4b5b418bde8c53df2dd4
Syed Azeez <syedazeez337@gmail.com>
2024-10-16 12:24:33 +0530

    New functions

---
+++ Solution_01/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"encoding/csv"
+	"flag"
+	"fmt"
+	"os"
+)
+
+func main() {
+	csvFilename := flag.String("csv", "questions.csv", "a csv file in the format of question, answer")
+	flag.Parse()
+
+	file, err := os.Open(*csvFilename)
+	if err != nil {
+		exit(fmt.Sprintf("Failed to open the CSV file: %s\n", *csvFilename))
+	} /*else {
+		fmt.Println("Success")
+	}*/
+	r := csv.NewReader(file)
+
+	lines, err := r.ReadAll()
+	if err != nil {
+		exit(fmt.Sprintln("Failed to parse the provided CSV file"))
+	}
+
+	problems := parseLines(lines)
+	for i, val := range problems {
+		fmt.Printf("Problem #%d: %s = \n", i+1, val.ques)
+		var answer string
+		fmt.Scanf("%s\n", &answer)
+		if answer == val.ans {
+			fmt.Println("Correct")
+		} else {
+			fmt.Println("Incorrect")
+		}
+	}
+}
+
+func parseLines(lines [][]string) []problem {
+	ret := make([]problem, len(lines))
+
+	for i, line := range lines {
+		ret[i] = problem{
+			ques: line[0],
+			ans: line[1],
+		}
+	}
+	return ret
+}
+
+// a new type of problems
+type problem struct {
+	ques string
+	ans string
+}
+
+func exit(msg string) {
+	fmt.Println(msg)
+	os.Exit(1)
+}
